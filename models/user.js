121f48771f9daf7a567b0a1fb1b53c13ec601d68
desertalex <alexbraddock22@gmail.com>
2021-02-25 14:31:18 -0700

    Initial basic user model created

---
+++ models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    resetToken: String,
+    resetTokenExpiration: Date
+    /* Add game object here in user
+    game: {
+        items: [
+            {
+                productId: { type: Schema.Types.ObjectId, ref: 'Product', required: true },
+                quantity: { type: Number, required: true }
+            }
+        ]
+    }
+    */
+});
+
+module.exports = mongoose.model('User', userSchema);
