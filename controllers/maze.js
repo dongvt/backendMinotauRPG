35c8313f45d63d533f0fef8fb6961962d57bcd26
Tyler Peart <tyler.e.peart@gmail.com>
2021-03-02 18:56:07 -0700

    Maze generation now functioning

--- controllers/maze.js
+++ controllers/maze.js
@@ -5,8 +5,9 @@ var cells = {};
 var walls = {};
 var backup;
 
+var msize;
 var rwall, cwtag;
-var nwalls;
+var nwalls = 0;
 var potentialWalls = [];
 
 function newmaze() {
@@ -16,7 +17,7 @@ function newmaze() {
     potentialWalls = [];
 
     //build cell objects
-    for (var r = 0; r < msize; c++) {
+    for (var r = 0; r < msize; r++) {
         for (var c = 0; c < msize; c++) {
             var ct = ""+ (r*msize+c);
 
@@ -67,11 +68,11 @@ function next() {
 
     if( cells[rwn1].setindex != cells[rwn2].setindex ) {
         //not in same set, remove wall
-        walls[rwalll][0] = false;
+        walls[rwall][0] = false;
         potentialWalls.splice(potentialWalls.indexOf(rwall),1);
 
         //add all cells in second set to first set
-        var secondSetIndex = cells[rw2].setindex;
+        var secondSetIndex = cells[rwn2].setindex;
         var newSetIndex = cells[rwn1].setindex;
         const keys = Object.keys(cells) //no semicolon in original doc
         for (const key of keys) {
