47df5c3aff8c23541e9714bf7a226ae3bd2205fa
Govert <car16116@byui.edu>
2021-03-08 18:18:55 -0700

    Heroku Test

--- app.js
+++ app.js
@@ -32,8 +32,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req,res,next) => {
     res.setHeader('Access-Control-Allow-Origin','*');
-    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
-    res.setHeader('Access-Control-Allow-Headers','Content-Type,Authorization');
+    res.setHeader('Access-Control-Allow-Methods','GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers','Content-Type, Authorization');
     next();
 });
 
