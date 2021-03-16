0ad2112a5efeffe6022ba8c4276af7897770c098
desertalex <alexbraddock22@gmail.com>
2021-03-16 15:25:32 -0700

    newGame fixed. saveGame added.

--- controllers/game.js
+++ controllers/game.js
@@ -11,6 +11,62 @@ const mongoose = require('mongoose');
 exports.patchNewGame = (req,res,next) => {
     const x = req.body.h;
     const y = req.body.w;
+    const maze = new Maze(x,y);
+
+    const game = new Game({
+        maze: maze.convert(),
+        userIndex: 11, //To change, would be a random number from 0 to matrix.lengh - 1
+        enemyList: [] //I asume is a list of numbers with the indexes of the position of each enemy
+    });
+    res.json(game);
+};
+
+exports.putLoadGame = (req,res,next) =>{
+    const gameId = req.body.gameId;
+    console.log('gameId: ' + gameId);
+    Game.findById(gameId).then(maze => {
+        res.json(maze);
+    }).catch(err => {
+        console.log(err);
+        res.json({status: 500, message:'Something went wrong loading the game'} );
+    });
+}
+
+exports.postSaveGame = (req, res, next) => {
+    const maze = req.body.maze;
+    const enemyList = req.body.enemyList;
+    const _id = req.body._id;
+    const userIndex = req.body.userIndex;
+    const userId = req.body.playerId;
+
+    const game = new Game({
+        maze: maze,
+        userIndex: userIndex,
+        enemyList: enemyList 
+    });
+    game.save()
+    .then(result => {
+        newGame = result;
+        return User.findById(userId)
+    })
+    .then(user => {
+        if (!user.games)
+            user.games = [];
+        
+        user.games.push(newGame);
+        user.save();
+        res.json({status: 200, message:'Game saved'});
+    })
+    .catch(err => {
+        console.log(err)
+        res.json({status: 500, message:'Something went wrong saving the game'} );
+    });
+}
+
+/* Old patchNewGame
+exports.patchNewGame = (req,res,next) => {
+    const x = req.body.h;
+    const y = req.body.w;
     const userId = req.body.userId;
     const maze = new Maze(x,y);
 
@@ -39,21 +95,4 @@ exports.patchNewGame = (req,res,next) => {
         res.json({status: 500, message:'Something went wrong saving the game'} );
     })
 }
-
-exports.putLoadGame = (req,res,next) =>{
-    const gameId = req.body.gameId;
-    console.log('gameId: ' + gameId);
-    var idExists = false;
-    Game.findById(gameId).then(maze => {
-        idExists = true;
-        res.json(maze);
-    }).catch(err => {
-        console.log(err);
-        res.json({status: 500, message:'Something went wrong loading the game'} );
-    });
-}
-
-exports.postSaveGame = (req, res, next) => {
-    const game = req.body.game;
-    // all other code
-}
+*/
