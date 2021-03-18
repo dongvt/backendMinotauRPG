4c7de66ae9eddde93e8a2030d60a8afb06db0385
desertalex <alexbraddock22@gmail.com>
2021-03-18 11:09:51 -0700

    saveGame function fixed

--- controllers/game.js
+++ controllers/game.js
@@ -8,10 +8,10 @@ const Game = require('../models/game');
 // Mongoose
 const mongoose = require('mongoose');
 
-exports.patchNewGame = (req,res,next) => {
+exports.patchNewGame = (req, res, next) => {
     const x = req.body.h;
     const y = req.body.w;
-    const maze = new Maze(x,y);
+    const maze = new Maze(x, y);
 
     const game = new Game({
         maze: maze.convert(),
@@ -21,46 +21,77 @@ exports.patchNewGame = (req,res,next) => {
     res.json(game);
 };
 
-exports.putLoadGame = (req,res,next) =>{
+exports.putLoadGame = (req, res, next) => {
     const gameId = req.body.gameId;
     console.log('gameId: ' + gameId);
     Game.findById(gameId).then(maze => {
         res.json(maze);
     }).catch(err => {
         console.log(err);
-        res.json({status: 500, message:'Something went wrong loading the game'} );
+        res.json({ status: 500, message: 'Something went wrong loading the game' });
     });
 }
 
 exports.postSaveGame = (req, res, next) => {
     const maze = req.body.maze;
     const enemyList = req.body.enemyList;
-    const _id = req.body._id;
+    const id = req.body._id;
     const userIndex = req.body.userIndex;
     const userId = req.body.playerId;
 
-    const game = new Game({
-        maze: maze,
-        userIndex: userIndex,
-        enemyList: enemyList 
-    });
-    game.save()
-    .then(result => {
-        newGame = result;
-        return User.findById(userId)
+
+    Game.countDocuments({ _id: id }, function (err, count) {
+        if (count > 0) { // game exists so update game
+            console.log(id);
+            Game.findByIdAndUpdate(
+                id,
+                {
+                    maze: maze,
+                    userIndex: userIndex,
+                    enemyList: enemyList
+                }
+            ).then(result => {
+                res.json({ status: 200, message: 'Game saved' });
+            }).catch(err => {
+                console.log(err);
+                res.json({ status: 500, message: 'Something went wrong saving the game' });
+            })
+        } else { // save game as new game
+            const game = new Game({
+                maze: maze,
+                userIndex: userIndex,
+                enemyList: enemyList,
+                _id: id
+            });
+            game.save()
+                .then(result => {
+                    newGame = result;
+                    return User.findById(userId)
+                })
+                .then(user => {
+                    if (!user.games) {
+                        user.games = [];
+                    }
+
+                    user.games.push(newGame);
+                    user.save();
+                    res.json({ status: 200, message: 'Game saved' });
+                })
+                .catch(err => {
+                    console.log(err)
+                    if (!userId) {
+                        res.json({ status: 500, message: 'playerId field is missing' });
+                    } else {
+                        res.json({ status: 500, message: 'Something went wrong saving the game' });
+                    }
+                });
+        }
+    }).catch(err => {
+        res.json({ status: 500, message: 'Something went wrong saving the game' });
     })
-    .then(user => {
-        if (!user.games)
-            user.games = [];
-        
-        user.games.push(newGame);
-        user.save();
-        res.json({status: 200, message:'Game saved'});
-    })
-    .catch(err => {
-        console.log(err)
-        res.json({status: 500, message:'Something went wrong saving the game'} );
-    });
+
+
+
 }
 
 /* Old patchNewGame
@@ -85,7 +116,7 @@ exports.patchNewGame = (req,res,next) => {
     .then(user => {
         if (!user.games)
             user.games = [];
-        
+
         user.games.push(newGame); // get rid of this
         user.save(); // get rid of this
         res.json(newGame);
